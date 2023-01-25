6e9680313741565025fd7f2b237f8bcb7617f93a
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2023-01-25 10:33:30 +0100

    update

--- doc/src/week1/make.sh
+++ doc/src/week1/make.sh
@@ -44,37 +44,12 @@ system doconce split_html $html.html --method=space10
 # Bootstrap style
 html=${name}-bs
 system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
-system doconce split_html $html.html --method=split --pagination --nav_button=bottom
+#system doconce split_html $html.html --method=split --pagination --nav_button=bottom
 
 # IPython notebook
-system doconce format ipynb $name $opt  --no_abort
-
-# LaTeX Beamer slides
-beamertheme=red_plain
-system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-system doconce slides_beamer $name --beamer_slide_theme=$beamertheme
-system pdflatex -shell-escape ${name}
-system pdflatex -shell-escape ${name}
-cp $name.pdf ${name}-beamer.pdf
-cp $name.tex ${name}-beamer.tex
-
-# Handouts
-system doconce format pdflatex $name --latex_title_layout=beamer --latex_table_format=footnotesize $opt
-system doconce ptex2tex $name envir=minted
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-system doconce slides_beamer $name --beamer_slide_theme=red_shadow --handout
-system pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-pdfnup --nup 2x3 --frame true --delta "1cm 1cm" --scale 0.9 --outfile ${name}-beamer-handouts2x3.pdf ${name}.pdf
-rm -f ${name}.pdf
+system doconce format ipynb $name $opt
 
 # Ordinary plain LaTeX document
-rm -f *.aux  # important after beamer
 system doconce format pdflatex $name --minted_latex_style=trac --latex_admon=paragraph $opt
 system doconce ptex2tex $name envir=minted
 # Add special packages
@@ -83,7 +58,8 @@ doconce replace 'section{' 'section*{' $name.tex
 pdflatex -shell-escape $name
 pdflatex -shell-escape $name
 mv -f $name.pdf ${name}.pdf
-cp $name.tex ${name}-plain.tex
+cp $name.tex ${name}.tex
+
 
 
 
@@ -116,3 +92,7 @@ EOF
 tar czf ${ipynb_tarfile} README.txt
 fi
 cp ${ipynb_tarfile} $dest/$name/ipynb
+
+
+
+
