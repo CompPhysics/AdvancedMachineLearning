9822e614fe1b29aa23fc5d338517705395d9ae4c
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2024-02-13 21:31:19 -0500

    update

---
+++ doc/src/week5/programs/svd.py
@@ -0,0 +1,59 @@
+from matplotlib.image import imread
+import matplotlib.pyplot as plt
+import scipy.linalg as ln
+import numpy as np
+import os
+from PIL import Image
+from math import log10, sqrt 
+plt.rcParams['figure.figsize'] = [16, 8]
+# Import image
+A = imread(os.path.join("photo.jpg"))
+X = A.dot([0.299, 0.5870, 0.114]) # Convert RGB to grayscale
+img = plt.imshow(X)
+# Konvert to gray
+img.set_cmap('gray')
+plt.axis('off')
+plt.show()
+# Call image size
+print(': %s'%str(X.shape))
+
+
+# split the matrix into U, S, VT
+U, S, VT = np.linalg.svd(X,full_matrices=False)
+S = np.diag(S)
+m = 800 # Image's width
+n = 1200 # Image's height
+j = 0
+# Try compression with different k:
+for k in (5,10, 20, 100,200,400,500):
+    # Original size of the image
+    originalSize = m * n 
+    # Size after compressed
+    compressedSize = k * (1 + m + n) 
+    # Aüürpximation of A
+    Xapprox = U[:,:k] @ S[0:k,:k] @ VT[:k,:]
+    plt.figure(j+1)
+    j += 1
+    img = plt.imshow(Xapprox)
+    img.set_cmap('gray')
+    
+    plt.axis('off')
+    plt.title('k = ' + str(k))
+    plt.show() 
+    print('Originalgröße:')
+    print(originalSize)
+    print('komprimierte Größe / Originalgröße:')
+    # Estimate the compression rate
+    ratio = compressedSize * 1.0 / originalSize
+    print(ratio)
+    print('Compression rate is ' + str( round(ratio * 100 ,2)) + '%' )  
+    # Estimate MQA
+    x= X.astype("float")
+    y=Xapprox.astype("float")
+    err = np.sum((x - y) ** 2)
+    err /= float(X.shape[0] * Xapprox.shape[1])
+    print('Die Mittlere quadratische Abweichung beträgt '+ str(round( err)))
+    max_pixel = 255.0
+    # Estimate Signal Noise Ratio
+    srv = 20 * (log10(max_pixel / sqrt(err)))
+    print('Signal-Rausch-Verhältnis beträgt '+ str(round(srv)) +'dB')
