64610314133ce8c768b72bedeae483d4c64a034c
Morten Hjorth-Jensen <morten.hjorth-jensen@fys.uio.no>
2026-02-12 07:28:12 +0100

    update

--- doc/src/Projects/2026/Project1/make.sh
+++ doc/src/Projects/2026/Project1/make.sh
@@ -24,38 +24,15 @@ opt=
 rm -f *.aux
 
 
-
-# Plain HTML documents
-html=${name}
-system doconce format html $name --pygments_html_style=default --html_style=bloodish --html_links_in_new_window --html_output=$html $opt
-system doconce split_html $html.html --method=space10
-
-# Bootstrap style
-html=${name}-bs
-system doconce format html $name --html_style=bootstrap --pygments_html_style=default --html_admon=bootstrap_panel --html_output=$html $opt
-system doconce split_html $html.html --method=split --pagination --nav_button=bottom
-
 # IPython notebook
 system doconce format ipynb $name $opt
 
 
-# Ordinary plain LaTeX document
-system doconce format pdflatex $name --print_latex_style=trac --latex_admon=paragraph $opt
-system doconce ptex2tex $name envir=verbatim
-# Add special packages
-doconce subst "% Add user's preamble" "\g<1>\n\\usepackage{simplewick}" $name.tex
-doconce replace 'section{' 'section*{' $name.tex
-pdflatex -shell-escape $name
-pdflatex -shell-escape $name
-mv -f $name.pdf ${name}.pdf
-cp $name.tex ${name}.tex
 
 # Publish
 dest=../../../../Projects/2026
 if [ ! -d $dest/$name ]; then
 mkdir $dest/$name
-mkdir $dest/$name/pdf
-mkdir $dest/$name/html
 mkdir $dest/$name/ipynb
 fi
 cp ${name}*.tex $dest/$name/pdf
